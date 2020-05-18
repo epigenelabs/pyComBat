b8ddcc7032f6bbc7aef90688594014754021e9ff
Abdelkader Behdenna <abdelkader@epigenelabs.com>
2020-05-18 15:48:51 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="combat",
-    version="0.1.5",
+    version="0.1.7",
     author="Abdelkader Behdenna",
     author_email="abdelkader@epigenelabs.com",
     description="pyComBat, a Python tool for batch effects correction in high-throughput molecular data using empirical Bayes methods",
