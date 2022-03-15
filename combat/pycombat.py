3c74978e86bd52ed8814336514a857db9854aaab
abdelkaderbehdenna <56023232+abdelkaderbehdenna@users.noreply.github.com>
2022-03-15 16:07:11 +0100

    Update pycombat.py

--- combat/pycombat.py
+++ combat/pycombat.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------
-# Copyright (C) 2019-2020 A. Behdenna, A. Nordor, J. Haziza and A. Gema
+# Copyright (C) 2019-2022 A. Behdenna, A. Nordor, J. Haziza and A. Gema
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
